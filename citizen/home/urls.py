446b35aaae2adf368ae631f86f073ddd9f9a9fbb
chaitanyathakare123 <chaitanya.thakare23@comp.sce.edu.in>
2024-03-30 03:54:50 +0530

    Initial commit

---
+++ citizen/home/urls.py
@@ -0,0 +1,15 @@
+
+from django.contrib import admin
+from django.urls import path
+from home import views
+
+urlpatterns = [
+    path("",views.index,name='home'),
+    path("about",views.about,name='about'),
+    path("services",views.services,name='services'),
+    path("track",views.track,name='track'),
+    path("contact",views.contact,name='contact'),
+    path("login",views.login,name='login'),
+    path('register/', views.register, name='register'),
+    path("logout",views.logout,name='logout')
+]
---
+++ citizen/home/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render,HttpResponse,redirect
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.views import LoginView
+from .forms import register
+
+# Create your views here.
+def index(request):
+    context={
+        
+    }
+    return render(request,'index.html',context)
+    #return HttpResponse("this is homepage")
+
+def about(request):
+    return render(request,'about.html')
+    #return HttpResponse("this is about page")
+
+def services(request):
+    return render(request,'services.html')
+    #return HttpResponse("this is services page")
+
+def track(request):
+    return render(request,'track.html')
+
+def contact(request):
+    if request.method=="POST":
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        phone=request.POST.get('phone')
+        desc=request.POST.get('desc')
+        contact=Contact(name=name,email=email,phone=phone,desc=desc,date=datetime.today())
+        contact.save()
+        messages.success(request,'your message as been send!')
+    return render(request,'contact.html')
+    #return HttpResponse("this is contact page")
+
+def login(request):
+  return render(request,'login.html')
+
+def register(request):
+  return render(request,'register.html')
+
+def logout(request):
+
+    return render(request,'logout.html')
